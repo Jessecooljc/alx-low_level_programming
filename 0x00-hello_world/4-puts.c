288dcbc226a03227126ba945f70542e4006675ef
jessecooljc <oluwagbemijesse@gmail.com>
2022-09-08 06:09:43 -0700

    kkkkk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
  *
  *  * main - main block
@@ -8,13 +6,8 @@
  *   * Return: 0
  *
  *    */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
