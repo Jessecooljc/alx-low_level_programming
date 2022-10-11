7b1a8cf7f997c67e3d1421672b94600e7a2ea2fa
jessecooljc <oluwagbemijesse@gmail.com>
2022-10-11 02:43:25 -0700

    I think I fixed it

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,26 +1,16 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * print_dog - prints a struct dog
  * @d: pointer
  * Return: nothing
  */
-void print_dog(struct dog *d)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: %s\n", "(nil)");
-		if (d->age >= 0)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: %s\n", "(nil)");
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: %s\n", "(nil)");
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
